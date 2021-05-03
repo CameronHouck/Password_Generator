b9627a95dd057411b1814d3291c6b9cbde54bd06
Cameron Houck <cameronhouck1@gmail.com>
2021-05-03 18:17:35 -0400

    adding character variables

---
+++ assets/script.js
@@ -0,0 +1,5 @@
+//List of variables being the characters used in the password
+var specialCharacters = "`~!@#$%^&*()[]{};:<>?/";
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numbers = "1234567890";
