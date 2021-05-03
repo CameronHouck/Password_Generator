ab0a6e646203905f50c2a4da790e3d41efee281c
Cameron Houck <cameronhouck1@gmail.com>
2021-05-03 18:20:01 -0400

    making generate button clickable

--- assets/script.js
+++ assets/script.js
@@ -3,3 +3,6 @@ var specialCharacters = "`~!@#$%^&*()[]{};:<>?/";
 var lowerCase = "abcdefghijklmnopqrstuvwxyz";
 var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var numbers = "1234567890";
+//Making the generae button clickable
+var generateBtn = document.querySelector("#generate");
+generateBtn.addEventListener("click", generatePassword);
