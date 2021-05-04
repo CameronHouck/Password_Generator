3dc4d9ff238dfffba63249c2f5514f237d8ca8b4
Cameron Houck <cameronhouck1@gmail.com>
2021-05-03 20:35:18 -0400

    adding concat user input logic

--- assets/script.js
+++ assets/script.js
@@ -17,7 +17,7 @@ function createPassword(word) {
 
 function generatePassword() {
   var userChoice = "";//empty space to be filled according to user choice.
-  var passLength = parseInt(window.prompt("How Many characters do you want?")); // converts user choice into an integer.
+  var passLength = parseInt(window.prompt("How Many characters do you want (between 8 and 128)?")); // converts user choice into an integer.
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
   console.log(passLength);
@@ -36,11 +36,42 @@ else if (passLength < 8 ) { //prompt to tell you password must be over 8 charact
 }
 
 else { //Prompts that ask you what to include your password.
-  var isSpecChosen = window.confirm("Press OK to include special characters, otherwise press cancel");
+  var isSpecChosen = window.confirm("Press OK to include special characters, otherwise press cancel.");
   console.log(isSpecChosen);
-  var isLowerCase = window.confirm("Press OK to include lowercase letters, otherwise press cancel");
+  var isLowerCase = window.confirm("Press OK to include lowercase letters, otherwise press cancel.");
   console.log(isLowerCase);
-  var isUpperCase = window.confirm("Press OK to include uppercase letters, otherwise press cancel");
+  var isUpperCase = window.confirm("Press OK to include uppercase letters, otherwise press cancel.");
   console.log(isUpperCase);
-  var isNumber = window.confirm("Press OK to include numbers, otherwise press cancel");
+  var isNumber = window.confirm("Press OK to include numbers, otherwise press cancel.");
+}
+//disables user from not choosing any characters and presenting them with a message.
+if (!isSpecChosen && !isLowerCase && !isUpperCase && !isNumber) { 
+  alert("Must choose a character type!");
+  return;
+
+}
+//user choice logic, and concatinating users choice into a generated password.
+if (isSpecChosen){ 
+  userChoice = userChoice.concat(specialCharacters); 
+}
+if (isLowerCase){
+  userChoice = userChoice.concat(lowerCase);
+}
+if (isUpperCase){
+  userChoice = userChoice.concat(upperCase);
+}
+if (isNumber){
+  userChoice = userChoice.concat(numbers);
+}
+
+function writePassword(length, choice) {
+  console.log("goodnight", length, choice)
+
+
+  var password = "";
+for ( var i = 0; i < length; i++) { // for loop to make password based off of the user input.
+  password += choice.charAt(Math.floor(Math.random() * choice.length));
+  }  
+  createPassword(password); //finally displays password in the box.
+}
 }
